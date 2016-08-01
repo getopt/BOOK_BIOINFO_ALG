6b4b3ee73c88bb6e6a4960764016890b3a28c83c
U-ILLUMINA\smanakov <smanakov@USSD-OFCDT6545.illumina.com>
2016-08-01 09:12:20 -0700

    init upload

---
+++ BioinfoAlgorithms/BioinfoAlgorithms/Chapter01.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BioinfoAlgorithms
+{
+    class Chapter01
+    {
+        public int PatternToNumber(string pattern)
+        {
+            if (string.IsNullOrEmpty(pattern))
+            {
+                return 0;
+            }
+
+            return 1;
+        }
+    }
+}
---
+++ BioinfoAlgorithms/BioinfoAlgorithms/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BioinfoAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Chapter01 chapter = new Chapter01();
+
+            int result = chapter.PatternToNumber("foo");
+
+            Console.WriteLine(result);
+            var name = Console.ReadLine();
+
+        }
+    }
+}
